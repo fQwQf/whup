fbeb979a778e23363f342a1c9177819036bf1ad4
mr-gentle-666 <965845592@qq.com>
2024-11-16 15:11:52 +0800

    while类初步实现

---
+++ include/while.h
@@ -0,0 +1,10 @@
+extern std::vector<ThreeAddressCode> tacs;
+class While
+{
+	Expr* While_Expr;
+	Block* While_Block;
+
+	Environment* While_env;
+public:
+	While(std::vector<Token>& code, Environment* env);
+};
