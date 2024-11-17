bd1f99db6dbd9b4f01c70cf292eccb73ded57860
mr-gentle-666 <965845592@qq.com>
2024-11-17 17:07:02 +0800

    if未完待续

--- include/if.h
+++ include/if.h
@@ -5,9 +5,11 @@ class If
 {
 	std::vector<Expr*>If_Expr;//可能有一系列ifelse，但是无所谓，一个expression控制一个block
 	std::vector<Block*>If_Block;
-	If* If_else;//采用嵌套方式，若有if后else部分则创建一个if的子节点
-
 	Environment* If_Env;
 public:
 	If(std::vector<Token>, Environment*);
+	void martchPar(int& i, std::vector<Token>code);
+	void martchBrace(int& i, std::vector<Token>code);
+
+	bool martchIfElse(int& i, std::vector<Token>code);//1表示继续，0表示else后不再有if
 };
--- src/if.cpp
+++ src/if.cpp
@@ -1,5 +1,23 @@
 #include"if.h"
-
+bool If::martchIfElse(int&i,std::vector<Token>code)
+{
+	if (code[i].type == KEYWORD && code[i].value == "if")
+	{
+		i++;
+		return 1;
+	}
+	else if (code[i].type == KEYWORD && code[i].value == "else")
+	{
+		i++;
+		if (code[i].type == KEYWORD && code[i].value == "if")
+		{
+			i++;
+			return 1;
+		}
+		else if (code[i].value == "{")
+			return 0;
+	}
+}
 
 If::If(std::vector<Token> code, Environment* env) :If_Env(env)
 {
@@ -34,6 +52,22 @@ If::If(std::vector<Token> code, Environment* env) :If_Env(env)
 	//If_Block = new Block(temp_If_Block, env);
 	////注意此时已经将三地址码压入tacs中
 
+	//临时变量用来在扫描过程中把code分别储存
+	std::vector<std::vector<Token>>If_Expr_temps;
+	std::vector<std::vector<Token>>If_Block_temps;
 	
+	int pos = 0;
+	while (pos < code.size())
+	{
+		//判断接下来的结构
+		bool isDone=martchIfElse(pos, code);
+
+		//记录一个expr
+		int Expr_begin = pos;
+		martchPar(pos, code);
+		If_Expr_temps.push_back(std::vector<Token>(code.begin() + Expr_begin, code.begin() + pos));
+
+		//记录一个block
+	}
 	
 }
