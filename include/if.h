6f745e0f4486e5e99333436acf0d13fd9d50c046
mr-gentle-666 <965845592@qq.com>
2024-11-17 15:39:20 +0800

    整体处理ifelse，一个expression对应一个block

--- include/if.h
+++ include/if.h
@@ -3,8 +3,8 @@
 extern std::vector<ThreeAddressCode>tacs;
 class If
 {
-	Expr* If_Expr;
-	Block* If_Block;
+	std::vector<Expr*>If_Expr;//可能有一系列ifelse，但是无所谓，一个expression控制一个block
+	std::vector<Block*>If_Block;
 	If* If_else;//采用嵌套方式，若有if后else部分则创建一个if的子节点
 
 	Environment* If_Env;
--- src/if.cpp
+++ src/if.cpp
@@ -1,58 +1,39 @@
-//#include"if.h"
-//
-//
-//If::If(std::vector<Token> code, Environment* env) :If_Env(env)
-//{
-//	//code.erase(code.begin());//预先将if删去
-//
-//	////设计label用于跳转
-//	//std::string If_Block_label = newTempLabel();
-//	//std::string nextIf = newTempLabel();
-//	////初始化W_expr;
-//	//std::vector<Token> temp_If_Expr;
-//	//int pos = 1;//不录入括号
-//	//while (code[pos].value != ")")
-//	//{
-//	//	temp_If_Expr.push_back(code[pos]);
-//	//	pos++;
-//	//}
-//	//If_Expr = new Expr(temp_If_Expr, env);
-//	////注意，此时已经将三地址码压入tacs中
-//
-//	////处理判断语句;
-//	//tacs.push_back({ "if_goto",If_Expr->getTacResult(),"",If_Block_label });//true则进入block
-//	//tacs.push_back({ "goto","","",nextIf});
-//
-//	////初始化W_block;
-//	//std::vector<Token> temp_If_Block;
-//	//pos += 2;//跳过）和{
-//	//while (code[pos].value != "}")
-//	//{
-//	//	temp_If_Block.push_back(code[pos]);
-//	//	pos++;
-//	//}
-//	//If_Block = new Block(temp_If_Block, env);
-//	////注意此时已经将三地址码压入tacs中
-//
-//	code.erase(code.begin());//预先将if删去
-//
-//	//设计label用于跳转
-//	std::string If_Block_label = newTempLabel();
-//	std::string If_end_label = newTempLabel();
-//	//初始化W_expr;
-//	std::vector<Token> temp_If_Expr;
-//	int pos = 1;//不录入括号
-//	while (code[pos].value != ")")
-//	{
-//		temp_If_Expr.push_back(code[pos]);
-//		pos++;
-//	}
-//	If_Expr = new Expr(temp_If_Expr, env);//得到结果
-//	tacs.push_back({ "if_goto",If_Expr->getTacResult(),"",If_Block_label });
-//	//注意，此时已经将三地址码压入tacs中
-//
-//	//接下来递归地解析if
-//
-//	If_else=new If()
-//	
-//}
+#include"if.h"
+
+
+If::If(std::vector<Token> code, Environment* env) :If_Env(env)
+{
+	//code.erase(code.begin());//预先将if删去
+
+	////设计label用于跳转
+	//std::string If_Block_label = newTempLabel();
+	//std::string nextIf = newTempLabel();
+	////初始化W_expr;
+	//std::vector<Token> temp_If_Expr;
+	//int pos = 1;//不录入括号
+	//while (code[pos].value != ")")
+	//{
+	//	temp_If_Expr.push_back(code[pos]);
+	//	pos++;
+	//}
+	//If_Expr = new Expr(temp_If_Expr, env);
+	////注意，此时已经将三地址码压入tacs中
+
+	////处理判断语句;
+	//tacs.push_back({ "if_goto",If_Expr->getTacResult(),"",If_Block_label });//true则进入block
+	//tacs.push_back({ "goto","","",nextIf});
+
+	////初始化W_block;
+	//std::vector<Token> temp_If_Block;
+	//pos += 2;//跳过）和{
+	//while (code[pos].value != "}")
+	//{
+	//	temp_If_Block.push_back(code[pos]);
+	//	pos++;
+	//}
+	//If_Block = new Block(temp_If_Block, env);
+	////注意此时已经将三地址码压入tacs中
+
+	
+	
+}
