c1f8ccb5e07bb979167899976f5b61b787bb1f82
fQwQf <supertjz123@foxmail.com>
2024-11-24 20:52:52 +0800

    feat(docs): 更新示例代码并增加错误处理
    - 在 docs/1.whup 中添加了 fib 函数示例，替换原有的 add 函数
    - 在 src/io.cpp 中增加了输入文件不存在时的错误处理
    - 在 src/parser.cpp 中添加了变量未找到时的错误提示

--- src/io.cpp
+++ src/io.cpp
@@ -3,6 +3,11 @@
 IO::IO(const std::string &in_file, const std::string &out_file)
 {
     in.open(in_file);
+
+    if(!in){
+        std::cout << "\033[0;31m Error(っ °Д °;)っ \033[0m " << in_file << ": no sucn file!" << std::endl;
+        exit(1);
+    }
     out.open(out_file);
 }
 
